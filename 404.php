e14e016fb5e8247893578dff04ac65ed89aa51bd
EC2 Default User <ec2-user@ip-172-31-16-40.us-east-2.compute.internal>
2019-12-11 18:10:59 +0000

    Pruebas de varios apectos de WordPress sobre un tema hijo

---
+++ 404.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the "site-content" div and all content after.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+
+get_header();
+?>
+
+	<section id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+			<div class="error-404 not-found">
+				<header class="page-header">
+					<h1 class="page-title" style="color : darkblue,font-size:50px"><?php _e( 'Estaras contento?, ya te has cargado el servidor', 'twentyfifteen' ); ?></h1>
+				</header><!-- .page-header -->
+
+				<div class="page-content">
+					<p><?php _e( 'Prueba a introducir bien la url, si no te esperas a que resolvamos tu estropicio', 'twentyfifteen' ); ?></p>
+					<?php get_search_form(); ?>
+				</div><!-- .page-content -->
+			</div><!-- .error-404 -->
+
+		</main><!-- #main -->
+	</section><!-- #primary -->
+
+<?php
+get_footer();
---
+++ functions.php
@@ -0,0 +1,32 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+ 
+    $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+ 
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+function themename_widgets_init() {
+    register_sidebar( array(
+        'name'          => __( 'Primary Sidebar', 'theme_name' ),
+        'id'            => 'sidebar-1',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</aside>',
+        'before_title'  => '<h3 class="widget-title">',
+        'after_title'   => '</h3>',
+    ) );
+ 
+    register_sidebar( array(
+        'name'          => __( 'Secondary Sidebar', 'theme_name' ),
+        'id'            => 'sidebar-2',
+        'before_widget' => '<ul><li id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</li></ul>',
+        'before_title'  => '<h3 class="widget-title">',
+        'after_title'   => '</h3>',
+    ) );
+}
